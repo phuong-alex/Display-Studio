b8f0a306fa7bbcb3c1b631e718f7dd01ade23fc8
phuong-alex <112266757+phuong-alex@users.noreply.github.com>
2026-08-05 02:08:33 +0700

    chore(v2): set Display Studio 2.0 foundation version

--- firmware/include/display_studio/version.h
+++ firmware/include/display_studio/version.h
@@ -4,8 +4,8 @@ namespace DisplayStudio::Version {
 inline constexpr const char* PRODUCT =
     "Display Studio Device";
 inline constexpr const char* RELEASE =
-    "Sprint 1.6";
+    "Display Studio 2.0 Foundation";
 inline constexpr const char* FIRMWARE =
-    "1.6.0-sprint.1.6";
+    "2.0.0-foundation.1";
 inline constexpr int PROTOCOL = 3;
 }
