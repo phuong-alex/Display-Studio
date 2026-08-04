df781e0f5be4f1b213d612194395ad4ae4365790
phuong-alex <112266757+phuong-alex@users.noreply.github.com>
2026-08-05 04:07:55 +0700

    chore(release): bump scalable project storage build

--- firmware/include/display_studio/version.h
+++ firmware/include/display_studio/version.h
@@ -4,8 +4,8 @@ namespace DisplayStudio::Version {
 inline constexpr const char* PRODUCT =
     "Display Studio Device";
 inline constexpr const char* RELEASE =
-    "Display Studio 3.0 Recoverable E-Ink Driver";
+    "Display Studio 3.0 Recoverable Driver";
 inline constexpr const char* FIRMWARE =
-    "3.0.0-recovery.1";
+    "3.0.1-project-blob.1";
 inline constexpr int PROTOCOL = 3;
 }
