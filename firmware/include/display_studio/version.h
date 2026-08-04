dd06ea216c5e5fb2aadccdaec97c6d00a3186ca9
phuong-alex <112266757+phuong-alex@users.noreply.github.com>
2026-08-05 00:31:15 +0700

    chore(release): bump Sprint 1.5.1 runtime fix

--- firmware/include/display_studio/version.h
+++ firmware/include/display_studio/version.h
@@ -4,8 +4,8 @@ namespace DisplayStudio::Version {
 inline constexpr const char* PRODUCT =
     "Display Studio Device";
 inline constexpr const char* RELEASE =
-    "Sprint 1.5";
+    "Sprint 1.5.1";
 inline constexpr const char* FIRMWARE =
-    "1.5.0-sprint.1.5";
+    "1.5.1-sprint.1.5.1";
 inline constexpr int PROTOCOL = 3;
 }
