0d6dcdbfaf2e1c4265c72f69a75c57e87e6f8f34
phuong-alex <112266757+phuong-alex@users.noreply.github.com>
2026-08-05 05:15:58 +0700

    chore(release): set streaming upload firmware version

--- firmware/include/display_studio/version.h
+++ firmware/include/display_studio/version.h
@@ -4,8 +4,8 @@ namespace DisplayStudio::Version {
 inline constexpr const char* PRODUCT =
     "Display Studio Device";
 inline constexpr const char* RELEASE =
-    "Display Studio 3.0 Recoverable Driver";
+    "Display Studio 3.0.2 Streaming Upload";
 inline constexpr const char* FIRMWARE =
-    "3.0.1-project-blob.1";
-inline constexpr int PROTOCOL = 3;
+    "3.0.2-streaming-upload.1";
+inline constexpr int PROTOCOL = 4;
 }
