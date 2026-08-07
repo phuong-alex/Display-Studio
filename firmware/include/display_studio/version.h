5cc19160313fb6cf7b065e562edf2d5ec9dd4cb4
phuong-alex <112266757+phuong-alex@users.noreply.github.com>
2026-08-07 20:26:27 +0700

    chore(version): mark LittleFS project storage build

--- firmware/include/display_studio/version.h
+++ firmware/include/display_studio/version.h
@@ -4,8 +4,8 @@ namespace DisplayStudio::Version {
 inline constexpr const char* PRODUCT =
     "Display Studio Device";
 inline constexpr const char* RELEASE =
-    "Display Studio 4.0 Core Storage Refactor";
+    "Display Studio 4.0 LittleFS Project Storage";
 inline constexpr const char* FIRMWARE =
-    "4.0.0-core-storage.1";
+    "4.0.1-littlefs-storage.1";
 inline constexpr int PROTOCOL = 4;
 }
